e57ab1d8265ef7596db891eafdfbceb923056dda
Gift-py <giftabumere247@gmail.com>
2023-05-27 07:42:12 +0100

    CRUD for tweets

--- TweeterBackend/src/routes/tweetRoutes.ts
+++ TweeterBackend/src/routes/tweetRoutes.ts
@@ -1,23 +1,53 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 // TWEETS CRUD
 //create tweet
-router.post('/', (req, res) => {
-    res.status(501).json({error: 'Not Implemented'})
+router.post('/', async(req, res) => {
+    const { content, image } = req.body;
+    // @ts-ignore
+    const user = req.user;
+
+    try {
+        const result = await prisma.tweet.create({
+            data: {
+                content,
+                image,
+                userId: user.id,
+            },
+            include: {user : true}
+        });
+
+        res.json(result)
+    } catch (e) {
+        res.status(400).json({error: 'Username and email should email should be unique'})
+    }
 });
 
 //get tweets
-router.get('/', (req, res) => {
-    res.status(501).json({error: 'Not Implemented'})
+router.get('/', async(req, res) => {
+    const allTweets = await prisma.tweet.findMany();
+    res.json(allTweets)
 })
 
 //get one tweet
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     const {id} = req.params;
-    res.status(501).json({error: `Not Implemented ${id}`})
-})
+    console.log('Query tweet with id: ', id)
+
+    const tweet = await prisma.tweet.findUnique({
+        where: {id:Number(id)},
+        include: {user:true},
+    });
+    if (!tweet) {
+        return res.status(404).json({ error: 'Tweet not found' });
+    }
+
+    res.json(tweet);
+});
 
 //update tweet
 router.put('/:id', (req, res) => {
@@ -26,10 +56,11 @@ router.put('/:id', (req, res) => {
 })
 
 //delete tweet
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
     const {id} = req.params;
-    res.status(501).json({error: `Not Implemented ${id}`})
-})
+    await prisma.tweet.delete({ where: { id: Number(id) }});
+    res.sendStatus(200);
+});
 
 
 export default router;
